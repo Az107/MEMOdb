9ca5c29f294f6e2673ce8f0016d4d2634dce2898
Alberto Ruiz <17555470+Az107@users.noreply.github.com>
2024-02-19 13:00:18 +0100

    Agregadas funciones de benchmark con parámetro de repetición

--- utils/benchmark.js
+++ utils/benchmark.js
@@ -2,35 +2,38 @@ const {MEMOdb} = require("../index.js");
 
 const memodb = new MEMOdb();
 
-function benchmark_write() {
+function benchmark_write(value) {
+  const repetitions = Math.pow(10, value);
   console.time("Write");
   memodb.create("test");
   const collection = memodb.get("test");
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < repetitions; i++) {
     collection.add({name: "test", index: i});
   }
   console.timeEnd("Write");
 }
 
-function benchmark_wirte_random() {
+function benchmark_wirte_random(value) {
+  const repetitions = Math.pow(10, value);
   console.time("Write Random");
   memodb.create("random");
   const collection = memodb.get("random");
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < repetitions; i++) {
     collection.add({name: "test", index: Math.random()});
   }
   console.timeEnd("Write Random");
 }
 
-function benchmark_read() {
+function benchmark_read(value) {
+  const repetitions = Math.pow(10, value);
   console.time("Read");
   const collection = memodb.get("test");
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < repetitions; i++) {
     collection.get(i);
   }
   console.timeEnd("Read");
 }
 
-benchmark_write();
-benchmark_wirte_random();
+benchmark_write(5);
+//benchmark_wirte_random();
 //benchmark_read();
